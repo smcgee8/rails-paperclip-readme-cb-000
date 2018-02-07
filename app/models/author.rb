424597bacb2e37ece7628a222473c892d01e53b9
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-07 20:05:46 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :posts
   has_attached_file :avatar
-  validates_attachment_content_type :avatar, :content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
